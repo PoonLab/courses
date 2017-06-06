f203ec1701a23ccdcd4490f0da474ad2cc7aa81c
ArtPoon <artpoon@gmail.com>
2017-06-06 11:34:31 -0400

    amended stream-bowtie2.py

--- GradPythonCourse/examples/stream-bowtie2.py
+++ GradPythonCourse/examples/stream-bowtie2.py
@@ -2,18 +2,24 @@ import subprocess
 import sys
 
 path = sys.argv[1]  # path to unpaired FASTQ file
-refpath = 'chr7'
+
+refpath = 'chr7'  # human chromosome 7
+
 outfile = path.replace('.fastq', '.sam')
 handle = open(outfile, 'w')
 
 p = subprocess.Popen(['bowtie2', '--quiet', '-x', refpath, '-U', path, '--local'], stdout=subprocess.PIPE)
+
 for line in p.stdout:
-    _, _, rname, _, mapq = line.split('\t')[:5]
     if line.startswith('@'):
         outfile.write(line)  # carry over header line
         continue
+    
+    _, _, rname, _, mapq = line.split('\t')[:5]
     if rname == 'chr7' and int(mapq) > 10:
         # only keep reads that mapped to chr7 with high quality
         outfile.write(line)
 
+outfile.close()
 handle.close()
+
