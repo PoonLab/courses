95209be8cfa4b900de90964336c7a9613b9a135b
Art Poon <artpoon@gmail.com>
2017-06-12 09:42:11 -0400

    commit before merge

---
+++ GradPythonCourse/examples/stream-bowtie2.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+
+path = sys.argv[1]  # path to unpaired FASTQ file
+refpath = 'chr7'
+outfile = path.replace('.fastq', '.sam')
+handle = open(outfile, 'w')
+
+p = subprocess.Popen(['bowtie2', '--quiet', '-x', refpath, '-U', path, '--local'], stdout=subprocess.PIPE)
+for line in p.stdout:
+    if line.startswith('@'):
+        handle.write(line)  # carry over header line
+        continue
+
+    _, _, rname, _, mapq = line.split('\t')[:5]
+    if rname == 'chr7' and int(mapq) > 10:
+        # only keep reads that mapped to chr7 with high quality
+        handle.write(line)
+
+handle.close()
+
+
